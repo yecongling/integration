b11f525617692bf8195b3bbd76d496fad838acb4
ycl <499475142@qq.com>
2022-11-30 09:34:49 +0800

    换用Hikari连接池

--- system/src/main/java/cn/soft/IntegrationApplication.java
+++ system/src/main/java/cn/soft/IntegrationApplication.java
@@ -3,12 +3,11 @@ package cn.soft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class IntegrationApplication extends SpringBootServletInitializer {
 
 
