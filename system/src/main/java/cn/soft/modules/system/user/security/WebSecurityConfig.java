53e91d62aa31061bf19a5e11ef57c9a59f9168ce
ycl <499475142@qq.com>
2022-12-05 12:54:25 +0800

    添加security的部分认证逻辑

---
+++ system/src/main/java/cn/soft/modules/system/user/security/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package cn.soft.modules.system.user.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @ClassName WebSecurityConfig
+ * @Description web安全验证配置
+ * @Author ycl
+ * @Date 2022/12/5 12:49
+ * @Version 1.0
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * Override this method to configure the {@link HttpSecurity}. Typically subclasses
+     * should not invoke this method by calling super as it may override their
+     * configuration. The default configuration is:
+     *
+     * <pre>
+     * http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+     * </pre>
+     * <p>
+     * Any endpoint that requires defense against common vulnerabilities can be specified
+     * here, including public ones. See {@link HttpSecurity#authorizeRequests} and the
+     * `permitAll()` authorization rule for more details on public endpoints.
+     *
+     * @param http the {@link HttpSecurity} to modify
+     * @throws Exception if an error occurs
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+    }
+}
