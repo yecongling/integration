0e814f6cdc9b2584b2f4d38d530ad299e1a1193f
ycl <499475142@qq.com>
2022-12-08 12:42:06 +0800

    搭建的react脚手架用作学习

--- react/react-staging/src/App.js
+++ react/react-staging/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Search } from './component/engine/search';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Search value="234"/>
     </div>
   );
 }
