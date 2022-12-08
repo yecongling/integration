73a3a4ec85d9c2853920b7d5f0ce19ad8a430a0e
yeconglin <499475142@qq.com>
2022-12-08 22:40:20 +0800

    学习脚手架开发

--- react/react-staging/src/App.js
+++ react/react-staging/src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { Search } from './component/engine/search';
+import { Hello } from './component/hello/Hello';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Search value="234"/>
+        <Hello/>
     </div>
   );
 }
-
+// 暴露app组件
 export default App;
---
+++ react/react-staging/src/component/hello/Hello.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import './hello.css'
+/* hello组件 */
+export class Hello extends Component {
+    render() {
+        return (
+            <div className="Hello">
+                Hello World
+            </div>
+        )
+    }
+}
