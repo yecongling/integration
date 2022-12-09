c6e71073804662340c4f2e782d80844932c5e262
ycl <499475142@qq.com>
2022-12-09 13:08:57 +0800

    引入antd

--- react/react-staging/src/component/Hello/Hello.js
+++ react/react-staging/src/component/Hello/Hello.js
@@ -1,11 +1,12 @@
 import React, {Component} from "react"
+import {Button} from "antd";
 import './hello.css'
 /* hello组件 */
 export class Hello extends Component {
     render() {
         return (
             <div className="Hello">
-                Hello World
+                <Button type="primary">点击我</Button>
             </div>
         )
     }
