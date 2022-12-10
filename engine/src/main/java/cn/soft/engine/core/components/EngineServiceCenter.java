7ff146eb90d497b0d1102c52c5ef0faefe620ffc
ycl <499475142@qq.com>
2022-12-10 12:11:59 +0800

    将引擎服务发布调用移动到engine工程

---
+++ engine/src/main/java/cn/soft/engine/core/components/EngineServiceCenter.java
@@ -0,0 +1,65 @@
+package cn.soft.engine.core.components;
+
+import cn.soft.common.api.vo.Result;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.camel.CamelContext;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName EngineServiceCenter
+ * @Description 引擎服务发布调用中心
+ * @Author ycl
+ * @Date 2022/12/10 10:50
+ * @Version 1.0
+ */
+@Component
+public class EngineServiceCenter implements BeanFactoryAware {
+
+    /**
+     * 注入camel运行的上下文环境，发布项目时，向该环境中添加运行路由
+     */
+    private CamelContext context;
+
+    @Autowired
+    public void setContext(CamelContext context) {
+        this.context = context;
+    }
+
+    /**
+     * bean工厂
+     */
+    private DefaultListableBeanFactory beanFactory;
+
+    /**
+     * Callback that supplies the owning factory to a bean instance.
+     * <p>Invoked after the population of normal bean properties
+     * but before an initialization callback such as
+     * {@link InitializingBean#afterPropertiesSet()} or a custom init-method.
+     *
+     * @param beanFactory owning BeanFactory (never {@code null}).
+     *                    The bean can immediately call methods on the factory.
+     * @throws BeansException in case of initialization errors
+     * @see BeanInitializationException
+     */
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = (DefaultListableBeanFactory) beanFactory;
+    }
+
+    /**
+     * 服务发布
+     *
+     * @param projectId 项目ID
+     * @return 服务发布结果
+     */
+    public Result<JSONObject> publishService(String projectId) {
+        return null;
+    }
+}
--- system/src/main/java/cn/soft/modules/engine/service/impl/ProjectManagerServiceImpl.java
+++ system/src/main/java/cn/soft/modules/engine/service/impl/ProjectManagerServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.soft.modules.engine.service.impl;
 
 import cn.soft.common.api.vo.Result;
+import cn.soft.engine.core.components.EngineServiceCenter;
 import cn.soft.modules.base.service.impl.BaseCommonServiceImpl;
 import cn.soft.modules.engine.entity.project.EndpointProperties;
 import cn.soft.modules.engine.entity.project.ProjectModel;
@@ -8,12 +9,7 @@ import cn.soft.modules.engine.entity.project.Route;
 import cn.soft.modules.engine.mapper.ProjectManagerMapper;
 import cn.soft.modules.engine.service.ProjectManagerService;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.camel.CamelContext;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -27,34 +23,7 @@ import java.util.List;
  * @Version 1.0
  **/
 @Service
-public class ProjectManagerServiceImpl extends BaseCommonServiceImpl implements ProjectManagerService, BeanFactoryAware {
-
-    /**
-     * 注入camel运行的上下文环境，发布项目时，向该环境中添加运行路由
-     */
-    private CamelContext context;
-
-    @Autowired
-    public void setContext(CamelContext context) {
-        this.context = context;
-    }
-
-    /**
-     * bean工厂
-     */
-    private DefaultListableBeanFactory beanFactory;
-
-    /**
-     * 注入bean工厂，作用是动态发布bean（如动态编译的cxf类，注册为bean，作为camel 的endpoint使用bean）
-     *
-     * @param beanFactory owning BeanFactory (never {@code null}).
-     *                    The bean can immediately call methods on the factory.
-     * @throws BeansException bean异常
-     */
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        this.beanFactory = (DefaultListableBeanFactory) beanFactory;
-    }
+public class ProjectManagerServiceImpl extends BaseCommonServiceImpl implements ProjectManagerService {
 
     /* 注入项目管理mapper */
     private ProjectManagerMapper projectManagerMapper;
@@ -62,6 +31,12 @@ public class ProjectManagerServiceImpl extends BaseCommonServiceImpl implements 
     @Autowired
     public void setProjectManagerMapper(ProjectManagerMapper projectManagerMapper) {
         this.projectManagerMapper = projectManagerMapper;
+    }
+
+    private EngineServiceCenter engineServiceCenter;
+    @Autowired
+    public void setEngineServiceCenter(EngineServiceCenter engineServiceCenter) {
+        this.engineServiceCenter = engineServiceCenter;
     }
 
     /**
@@ -119,6 +94,7 @@ public class ProjectManagerServiceImpl extends BaseCommonServiceImpl implements 
      * @return 发布的结果
      */
     public Result<JSONObject> publishService(String projectId) {
+        Result<JSONObject> result = engineServiceCenter.publishService(projectId);
         return null;
     }
 
