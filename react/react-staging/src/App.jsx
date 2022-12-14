196ba71e7f83c97f3184f28c6138dc7e8a833ad3
ycl <499475142@qq.com>
2022-12-14 12:56:48 +0800

    修改做测试接口

--- react/react-staging/src/App.jsx
+++ react/react-staging/src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css';
 import {Component} from "react";
 import axios from "axios";
+import {Button, Card, Input} from "antd";
 
 export default class App extends Component {
 
+    state = {
+        url: "",
+        data: ""
+    }
+
+    changeUrl = (event) => {
+        this.setState({url: event.target.value})
+    }
+
     getStudentData = () => {
-        axios.get('http://localhost:3000/integration/sys/project/endpointProperties').then(
+        const url = 'http://localhost:3000/integration';
+        axios.get(url + this.state.url).then(
             response => {
                 console.log('成功了', response);
+                this.setState({data: response.data})
             },
             error => {
                 console.log('失败了', error)
@@ -16,9 +28,15 @@ export default class App extends Component {
     }
 
     render() {
+        const {data} = this.state
         return (
             <div className="App">
-                <button onClick={this.getStudentData}>点我获取学生数据</button>
+                <Input type="text" size="large" onChange={this.changeUrl}/>
+                <Button type="primary" onClick={this.getStudentData}>点我获取学生数据</Button>
+                <span>接口的返回值是</span>
+                <Card title="返回值">
+                    <span>{JSON.stringify(data)}</span>
+                </Card>
             </div>
         )
     }
