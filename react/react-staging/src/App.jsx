f4ef75315d90cd75b15c25b3a1dfa21aed1596ba
yeconglin <499475142@qq.com>
2022-12-25 21:17:17 +0800

    学习路由

--- react/react-staging/src/App.jsx
+++ react/react-staging/src/App.jsx
@@ -1,22 +1,24 @@
 import {Component} from "react";
-import {Link, Route, Routes} from "react-router-dom";
-import About from './component/About';
-import Home from './component/Home';
+import {NavLink, Route, Routes, Navigate} from "react-router-dom";
+import About from './pages/About';
+import Home from './pages/Home';
+import Header from "./component/Header";
 export default class App extends Component {
     render() {
         return (
             <div className="container">
+                <Header/>
                 {/* 在React中靠路由链接实现切换组件 */}
-                    <Link className="list-group-item" to="/about">About</Link>
+                    <NavLink className="list-group-item" to="/about">About</NavLink>
                     <br/>
-                    <Link className="list-group-item" to="/home">Home</Link>
+                    <NavLink className="list-group-item" to="/home">Home</NavLink>
 
                 <div className="panel">
                     <Routes>
                         {/* 注册路由 */}
                         <Route path="/about" element={<About/>}/>
-                        <Route path="/" element={<About/>}/>
                         <Route path="/home" element={<Home/>}/>
+                        <Route path="/" element={<Navigate to="/home"/>}/>
                     </Routes>
                 </div>
             </div>
