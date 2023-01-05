89b2ead566f48d06086f3fe52148eb01087d293c
ycl <499475142@qq.com>
2023-01-05 14:38:53 +0800

    搭建框架

--- integration/src/App.tsx
+++ integration/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
         <Routes>
             <Route path="/login" element={<Login/>}/>
             <Route path="/register" element={<Register/>}/>
+            <Route path="/" element={<Login/>}/>
             <Route path="/index" element={!token ? <Navigate to="/login"/> : <BasicLayout/>}></Route>
         </Routes>
     );
