a166367f3fe4d6d39758fd71e7fc232437bfd58a
yeconglin <499475142@qq.com>
2023-01-14 21:46:57 +0800

    框架搭建

--- integration/src/pages/user/Login/index.tsx
+++ integration/src/pages/user/Login/index.tsx
@@ -58,7 +58,7 @@ const Login = (props: any) => {
                         autoComplete="off"
                     >
                         <Form.Item name="username" rules={[{required: true, message: "请输入用户名"}]}>
-                            <Input placeholder="用户名：admin" prefix={<UserOutlined/>}/>
+                            <Input autoFocus placeholder="用户名：admin" prefix={<UserOutlined/>}/>
                         </Form.Item>
                         <Form.Item name="password" rules={[{required: true, message: "请输入密码"}]}>
                             <Input.Password autoComplete="new-password" placeholder="密码：123456"
