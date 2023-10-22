fa1c8599c7105c5219d9a19b79ca163c0c56512a
ycl <499475142@qq.com>
2023-10-22 17:35:13 +0800

    添加文书种类的变更模块

---
+++ src/main/java/cn/net/integration/modules/emr/entity/EmrCategory.java
@@ -0,0 +1,15 @@
+package cn.net.integration.modules.emr.entity;
+
+/**
+ * @ClassName EmrCategory
+ * @Description 文书种类
+ * @Author ycl
+ * @Date 2023/10/22 17:33
+ * @Version 1.0
+ */
+public class EmrCategory {
+    /**
+     * 种类id
+     */
+    private String id;
+}
--- src/main/java/cn/net/integration/modules/emr/mapper/EmrDesignerMapper.java
+++ src/main/java/cn/net/integration/modules/emr/mapper/EmrDesignerMapper.java
@@ -1,6 +1,11 @@
 package cn.net.integration.modules.emr.mapper;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName DesignerMapper
@@ -11,4 +16,11 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface EmrDesignerMapper {
+
+    /**
+     * 查询所有的文书分类
+     * @param param 查询参数
+     * @return 分类信息
+     */
+    List<Map<String, Object>> getEmrCategory(@Param("param")JSONObject param);
 }
