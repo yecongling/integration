3e7814987e47b3b39c85a3edde1189d97376beb8
yeconglin <499475142@qq.com>
2023-12-28 14:29:10 +0800

    修改代码

--- src/main/java/cn/net/integration/IntegrationApplication.java
+++ src/main/java/cn/net/integration/IntegrationApplication.java
@@ -2,8 +2,10 @@ package cn.net.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class IntegrationApplication {
 
     public static void main(String[] args) {
