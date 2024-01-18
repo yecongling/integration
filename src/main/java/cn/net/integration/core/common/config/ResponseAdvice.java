a40588009c6773978bc91f9777ec32204b749465
ycl <499475142@qq.com>
2024-01-18 22:57:59 +0800

    加点注释

--- src/main/java/cn/net/integration/core/common/config/ResponseAdvice.java
+++ src/main/java/cn/net/integration/core/common/config/ResponseAdvice.java
@@ -19,11 +19,29 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 @ControllerAdvice
 public class ResponseAdvice implements ResponseBodyAdvice<Object> {
 
+    /**
+     * 是否支持该类型
+     *
+     * @param returnType    返回类型
+     * @param converterType 转化类型
+     * @return true 支持
+     */
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
         return true;
     }
 
+    /**
+     * 内容写入body
+     *
+     * @param body                  返回内容
+     * @param returnType            返回类型
+     * @param selectedContentType   类型
+     * @param selectedConverterType 类型
+     * @param request               请求
+     * @param response              响应
+     * @return 封装的返回类型
+     */
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
         if (body instanceof Result) {
