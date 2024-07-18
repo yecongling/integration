3a608415ac7f8c986b2cf09de4e9119275b2cffa
ycl <499475142@qq.com>
2024-07-18 10:43:27 +0800

    数据库连接接口层

---
+++ integration-module-engine/src/main/java/cn/net/engine/controller/DatabaseConnectionController.java
@@ -0,0 +1,18 @@
+package cn.net.engine.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName DatabaseConnectionController
+ * @Description 数据库连接接口
+ * @Author ycl
+ * @Date 2024/7/18 上午10:14
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/connection/database")
+public class DatabaseConnectionController {
+
+
+}
