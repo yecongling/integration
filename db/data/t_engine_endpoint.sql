0603bc1860907d81ea04b3b842f3c0760fcf853a
ycl <499475142@qq.com>
2024-08-12 10:54:22 +0800

    修改表结构，type->type_id，关联数据来源于t_engine_endpoint_type

--- db/data/t_engine_endpoint.sql
+++ db/data/t_engine_endpoint.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80039 (8.0.39-0ubuntu0.22.04.1)
  File Encoding         : 65001
 
- Date: 09/08/2024 10:56:15
+ Date: 12/08/2024 10:52:24
 */
 
 SET NAMES utf8mb4;
@@ -30,8 +30,8 @@ CREATE TABLE `t_engine_endpoint`
     `id`                  varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL COMMENT '端点唯一ID',
     `name`                varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '端点名字',
     `description`         varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '描述',
-    `project_id`          varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '所属项目ID（也可能同时属于多个项目，多个项目ID|分隔，也可能不属于任何项目，提前配置的）',
-    `type`                varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '类型（如http、soap、DB、FTP……）',
+    `project_id`          varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '所属项目ID（也可能同时属于多个项目，多个项目id|分隔，也可能不属于任何项目，提前配置的）',
+    `type_id`             varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL COMMENT '类型id - 关联t_engine_endpoint_type表（如http、soap、DB、FTP……）',
     `mode`                varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL COMMENT '模式，进出或进、出模式',
     `status`              int                                                           NOT NULL DEFAULT '1' COMMENT '状态 1-正常 2-部分异常 3-异常',
     `del_flag`            bit(1)                                                        NOT NULL DEFAULT b'0' COMMENT '删除标记 1 - 删除 0 - 未删除',
@@ -50,9 +50,9 @@ CREATE TABLE `t_engine_endpoint`
     `timeout`             int                                                           NOT NULL DEFAULT '0' COMMENT '连接或操作的超时时间',
     `retry_policy`        json                                                                   DEFAULT NULL COMMENT '重试策略配置，如重试次数、重试间隔等',
     `tags`                varchar(255) COLLATE utf8mb4_general_ci                                DEFAULT NULL COMMENT '为endpoint添加标签，便于分类和搜索',
-    PRIMARY KEY (`id`, `type`, `mode`) USING BTREE,
+    PRIMARY KEY (`id`, `type_id`, `mode`) USING BTREE,
     KEY `idx_endpoint_id` (`id`) USING BTREE,
-    KEY `idx_endpoint_type` (`type`) USING BTREE,
+    KEY `idx_endpoint_type` (`type_id`) USING BTREE,
     KEY `idx_endpoint_mode` (`mode`) USING BTREE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
